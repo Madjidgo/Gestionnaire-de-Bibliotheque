1b5977be3d8c2b0e2e07354a7829e9bf6cbaebc0
Madjidgo <madjidgo@gmail.com>
2017-12-04 23:49:12 +0100

    modification <i class=material-icons>speaker_notes</i>

--- resources/views/vendor/flashy/message.blade.php
+++ resources/views/vendor/flashy/message.blade.php
@@ -100,6 +100,7 @@
 @if(Session::has('flashy_notification.message'))
 <script id="flashy-template" type="text/template">
     <div class="flashy flashy--{{ Session::get('flashy_notification.type') }}">
+      <i class="material-icons">speaker_notes</i>
         <a href="#" class="flashy__body" target="_blank"></a>
     </div>
 </script>
